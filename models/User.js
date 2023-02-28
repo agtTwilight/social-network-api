1965437ded55ecf6b63f67a06728d4bbdaae38f7
agtTwilight <lukasmacmillen@gmail.com>
2023-02-27 21:04:26 -0800

    added email validation

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
   {
     username: {type:String, required:true, unique:true, trim:true},
-    email: {type:String, required:true, unique:true},
+    email: {type:String, required:true, unique:true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']},
     thoughts: [
       {
         type: Schema.Types.ObjectId,
